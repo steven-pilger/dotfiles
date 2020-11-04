4e9c098a882a7176c13bb0c47dd04b95695dc333
Steven Pilger <ptgdk@leo-pharma.com>
2020-11-04 11:41:12 +0100

    feat: fix conda path for fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,16 +9,21 @@ source ~/.environ
 export VISUAL=nvim
 export EDITOR="$VISUAL"
 set PATH $HOME/.poetry/bin/ $PATH
+set PATH $HOME/.pyenv/bin/ $PATH
 
 # VI Mode
 fish_vi_key_bindings
 set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
 
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-eval /Users/ptgdk/Documents/conda/miniconda3/bin/conda "shell.fish" "hook" $argv | source
-# <<< conda initialize <<<
-#
-#
-status --is-interactive; and source (pyenv init -|psub)
+set DEVICE (uname --nodename)
+if test $DEVICE = 'srv01312'
+    eval /compute/miniconda3/condabin/conda "shell.fish" "hook" $argv | source
+else
+    eval $HOME/Documents/conda/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+end
+
+# Pyenv
+status --is-interactive; and . (pyenv init -|psub)
+
+# Starship
 starship init fish | source
