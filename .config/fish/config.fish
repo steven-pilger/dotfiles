7e5ae6f71aa9b21712b835dfa00e0c7b5192970a
Steven Pilger <stevenpilger[at]fastmail[dot]com>
2020-11-15 14:27:26 +0100

    fix> fish path issues

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,6 +6,7 @@ export LANG=en_US.UTF-8
 source ~/.environ
 
 # DEFAULT EXPORTS
+# reset PATH
 export VISUAL=nvim
 export EDITOR="$VISUAL"
 set PATH $HOME/.poetry/bin/ $PATH
@@ -13,11 +14,12 @@ set PATH $HOME/.pyenv/bin/ $PATH
 
 # VI Mode
 fish_vi_key_bindings
-set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
 
 set DEVICE (uname -n)
 if test $DEVICE = 'srv01312'
     eval /compute/miniconda3/condabin/conda "shell.fish" "hook" $argv | source
+else if test $DEVICE = 'mbpro.wombat'
+    eval /usr/local/bin/conda "shell.fish" "hook" $argv | source
 else
     eval $HOME/Documents/conda/miniconda3/bin/conda "shell.fish" "hook" $argv | source
 end
