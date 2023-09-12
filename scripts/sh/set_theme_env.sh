d8646d61311abb81b0ba6fb5a71010667d2c440b
Steven Pilger <stevenpilger+github@fastmail.com>
2023-09-12 21:12:16 +0200

    feat: adjust script for theme switching to linux

--- scripts/sh/set_theme_env.sh
+++ scripts/sh/set_theme_env.sh
@@ -1,15 +1,32 @@
 #!/usr/bin/env bash
 
-# getting current theme and defaul working directory
-lwd=$pwd
-dark=$(defaults read -g AppleInterfaceStyle 2> /dev/null)
-if [[ $dark == *"Dark"* ]]; then
-	echo "System is in Dark mode"
-	dark=1
+# Determine the operating system
+if [[ "$(uname)" == "Darwin" ]]; then
+    # macOS
+    dark=$(defaults read -g AppleInterfaceStyle 2> /dev/null)
+    if [[ $dark == *"Dark"* ]]; then
+        echo "macOS is in Dark mode"
+        dark=1
+    else
+        echo "macOS is in Light mode"
+        dark=0
+    fi
+elif [[ "$(uname)" == "Linux" ]]; then
+    # Ubuntu/Linux
+    theme=$(gsettings get org.gnome.desktop.interface gtk-theme)
+    if [[ $theme == *"dark"* || $theme == *"night"* ]]; then
+        echo "Ubuntu/Linux is in Dark mode"
+        dark=1
+    else
+        echo "Ubuntu/Linux is in Light mode"
+        dark=0
+    fi
 else
-	echo "System is in Light mode"
-	dark=0
+    echo "Unsupported operating system"
+    exit 1
 fi
+
+lwd=$pwd
 
 # setting application themes appropriately
 cd ~/.config/kitty/
