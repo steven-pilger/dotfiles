6ce20e392bca1bd3d1432834058fe404321c8a30
STPI (Steven Pilger) <stpi@novozymes.com>
2024-01-17 09:04:06 +0100

    feat: style changes

--- .config/fish/functions/fkill.fish
+++ .config/fish/functions/fkill.fish
@@ -1,3 +1,3 @@
 function fkill
-    kill (ps ax --no-headers -o pid,comm | fzf --header "Select process to kill"| awk '{print $1}') $argv
+    kill (ps ax -o pid= -o comm= | fzf --header "Select process to kill"| awk '{print $1}') $argv
 end
